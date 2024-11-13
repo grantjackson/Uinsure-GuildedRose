c6315be3208c5130f05ab602c6a1ba7f06aa71a2
Grant Jackson <grant-j@hotmail.co.uk>
2024-11-13 23:07:50 +0000

    Add Conjured.cs and register in ItemFactory.cs

--- src/GildedRose/ItemFactory.cs
+++ src/GildedRose/ItemFactory.cs
@@ -8,12 +8,14 @@ public static class ItemFactory
     private const string AgedBrie = "Aged Brie";
     private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
     private const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+    private const string Conjured = "Conjured Mana Cake";
     
     public static BasicItem GetItem(Item item) => item.Name switch
     {
         AgedBrie => new AgedBrie(item),
         Sulfuras => new Sulfuras(item),
         BackstagePass => new BackstagePass(item),
+        Conjured => new Conjured(item),
         _ => new BasicItem(item)
     };
 }
---
+++ src/GildedRose/Models/Conjured.cs
@@ -0,0 +1,8 @@
+﻿using GildedRoseKata;
+
+namespace GildedRose.Models;
+
+public class Conjured(Item item) :  BasicItem(item)
+{
+    protected override int QualityIncrement => -2;
+}

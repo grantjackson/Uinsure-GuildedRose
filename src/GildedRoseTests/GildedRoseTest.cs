79e39df5afc5592037febb7cad5592f0980e2ce9
Grant Jackson <grant-j@hotmail.co.uk>
2024-11-13 22:07:48 +0000

    Move strings to constants

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -9,10 +9,10 @@ namespace GildedRoseTests
         [Fact]
         public void foo()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            IList<Item> items = new List<Item> { new() { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose.GildedRose app = new(items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal("foo", items[0].Name);
         }
     }
 }
